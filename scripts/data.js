f3bce1544e3e8df88a281d6987539855db834aac
hendrik3303 <hendrik.gonschor@student.uni-luebeck.de>
2026-01-18 22:18:06 +0100

    Initial commit

---
+++ scripts/data.js
@@ -0,0 +1,15 @@
+// Data file for all the amrkers
+const mapData = {
+    wracks: [
+        { id: 1, name: "Wrack in den Grassy Plateaus", coords: [-300, 150], info: "Enthält Laserschneider-Fragmente" },
+        { id: 2, name: "Wrack in den Blood Kelp Caves", coords: [-600, 400], info: "Vorsicht: Crab-Squids!" }
+    ],
+    blueprints: [
+        { id: 101, name: "Seaglide Fragment", coords: [-100, 50], info: "Im flachen Wasser zu finden" }
+    ],
+    lifepods: [
+        { id: 205, image: "images/lifepod5.jpg", name: "Lifepod 5 (Spawn)", coords: [0, 0], coordsinfo: "X:?,Y:2,Z:?" ,info: "Lifepod 5 will spawn randomly so this pin dosn't really matter" }
+    ]
+};
+
+//Lifepot 5 is at 24, 2, 284
---
+++ scripts/script.js
@@ -0,0 +1,84 @@
+const map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -2
+});
+
+const imgUrl = 'SubnauticaMap.png';
+const imgBounds = [[-1024, -1024], [1024, 1024]];
+
+L.imageOverlay(imgUrl, imgBounds).addTo(map);
+map.fitBounds(imgBounds);
+
+// Function to load the markers from the data.js
+function loadMarkers() {
+    mapData.wracks.forEach(wrack => {
+        L.marker(wrack.coords)
+            .addTo(map)
+            .bindPopup(`<b>${wrack.name}</b><br>${wrack.info}`);
+    });
+    mapData.lifepods.forEach(lifepod => {
+        let popupContent = "";
+
+        if (lifepod.image) {
+            popupContent += `<img src="${lifepod.image}" class="popup-img">`;
+        }
+
+        popupContent += `<b>${lifepod.name}</b>
+                        <hr style="border: 0.5px solid #233554; margin: 5px 0;">
+                        <small>${lifepod.coordsinfo}</small><br>
+                        <p>${lifepod.info}</p>`;
+
+        L.marker(lifepod.coords)
+            .addTo(map)
+            .bindPopup(popupContent, {
+            maxWidth: 300,
+            minWidth: 200, 
+            className: 'custom-popup'
+            })
+            .bindTooltip(lifepod.name, {
+            direction: 'right',
+            opacity: 0.9 
+            });
+    });
+}
+
+function getCoordinates() {
+    // Reference to HTML
+    const coordsDisplay = document.getElementById('coords-display');
+
+    // Event
+    map.on('mousemove', function(e) {
+        // Get Leaflet-cordinates
+        const { lat, lng } = e.latlng;
+
+        const gameX = Math.round(lng);
+        const gameZ = Math.round(lat);
+
+        // Refresh 
+        coordsDisplay.innerHTML = `X: ${gameX} | Z: ${gameZ}`;
+    });
+}
+
+function loadCustomleafletControll() {
+    map.whenReady(() => {
+    const zoomContainer = document.querySelector('.leaflet-control-zoom');
+
+    const centerBtn = L.DomUtil.create('a', 'leaflet-control-center', zoomContainer);
+    centerBtn.innerHTML = '⊙';
+    centerBtn.href = '#';
+    centerBtn.title = 'Center map';
+    centerBtn.role = 'button';
+
+    L.DomEvent.on(centerBtn, 'click', (e) => {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+        map.fitBounds(imgBounds);
+    });
+});
+}
+
+
+loadMarkers();
+getCoordinates();
+loadCustomleafletControll();
+
