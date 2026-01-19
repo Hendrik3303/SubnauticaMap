49ba386be735d78f6698b53c47daf0bc7da7f8db
hendrik3303 <hendrik.gonschor@student.uni-luebeck.de>
2026-01-19 20:35:52 +0100

    added tracking feature

--- scripts/data.js
+++ scripts/data.js
@@ -1,15 +1,17 @@
 // Data file for all the amrkers
 const mapData = {
     wracks: [
-        { id: 1, name: "Wrack in den Grassy Plateaus", coords: [-300, 150], info: "Enthält Laserschneider-Fragmente" },
-        { id: 2, name: "Wrack in den Blood Kelp Caves", coords: [-600, 400], info: "Vorsicht: Crab-Squids!" }
+        { id: "1", name: "Wrack in den Grassy Plateaus", coords: [-300, 150], info: "Enthält Laserschneider-Fragmente" },
+        { id: "2", name: "Wrack in den Blood Kelp Caves", coords: [-600, 400], info: "Vorsicht: Crab-Squids!" }
     ],
     blueprints: [
-        { id: 101, name: "Seaglide Fragment", coords: [-100, 50], info: "Im flachen Wasser zu finden" }
+        { id: "101", name: "Seaglide Fragment", coords: [-100, 50], info: "Im flachen Wasser zu finden" }
     ],
     lifepods: [
-        { id: 205, image: "images/lifepod5.jpg", name: "Lifepod 5 (Spawn)", coords: [0, 0], coordsinfo: "X:?,Y:2,Z:?" ,info: "Lifepod 5 will spawn randomly so this pin dosn't really matter" }
+        { id: "lp5", image: "images/lifepod5.jpg", name: "Lifepod 5 (Spawn)", coords: [0, 0], coordsinfo: "X:?,Y:2,Z:?" ,info: "Lifepod 5 will spawn randomly somewhere in the Safe Shallows, so this pin dosn't really matter" },
+        { id: "lp6", image: "images/lifepod6.jpg", name: "Lifepod 6", coords: [364, 312], coordsinfo: "X:364,Y:-113,Z:310", info: "You will find lifepod 6 at this location"}
     ]
 };
 
 //Lifepot 5 is at 24, 2, 284
+//lifepot 6 is at 364 -113 310 and ultra glide fins inside and also crew log 2 inside and #1 outside
--- scripts/script.js
+++ scripts/script.js
@@ -3,67 +3,81 @@ const map = L.map('map', {
     minZoom: -2
 });
 
+const allMarkers = {};
+
+const imgUrl = 'SubnauticaMap.png';
+const imgBounds = [[-2048, -2048], [2048, 2048]];
+L.imageOverlay(imgUrl, imgBounds).addTo(map);
+map.fitBounds(imgBounds);
+
 const lifepodIcon = L.icon({
     iconUrl: 'images/icons/lifepod.png',
     iconSize: [32, 32],
     iconAnchor: [16, 16],
-    popupAnchor: [0, -16],
+    popupAnchor: [-0.5, -16],
     tooltipAnchor: [16, 0]
 });
 
 
-const imgUrl = 'SubnauticaMap.png';
-const imgBounds = [[-1024, -1024], [1024, 1024]];
 
-L.imageOverlay(imgUrl, imgBounds).addTo(map);
-map.fitBounds(imgBounds);
+function toggleTrack(id, isChecked) {
+    localStorage.setItem('track-' + id, isChecked);
+    
+    if (allMarkers[id]) {
+        allMarkers[id].setOpacity(isChecked ? 0.4 : 1.0);
+    }
 
-// Function to load the markers from the data.js
-function loadMarkers() {
-    mapData.wracks.forEach(wrack => {
-        L.marker(wrack.coords)
-            .addTo(map)
-            .bindPopup(`<b>${wrack.name}</b><br>${wrack.info}`);
-    });
-    mapData.lifepods.forEach(lifepod => {
-        let popupContent = "";
-
-        if (lifepod.image) {
-            popupContent += `<img src="${lifepod.image}" class="popup-img">`;
-        }
-
-        popupContent += `<b>${lifepod.name}</b>
-                        <hr style="border: 0.5px solid #233554; margin: 5px 0;">
-                        <small>${lifepod.coordsinfo}</small><br>
-                        <p>${lifepod.info}</p>`;
-
-        L.marker(lifepod.coords, { icon: lifepodIcon })
-            .addTo(map)
-            .bindPopup(popupContent, {
-            maxWidth: 300,
-            minWidth: 200
-            })
-            .bindTooltip(lifepod.name, {
-            direction: 'right',
-            opacity: 0.9 
-            });
-    });
+    setTimeout(() => {map.closePopup();}, 200);
 }
 
 function getCoordinates() {
-    // Reference to HTML
     const coordsDisplay = document.getElementById('coords-display');
-
-    // Event
     map.on('mousemove', function(e) {
-        // Get Leaflet-cordinates
         const { lat, lng } = e.latlng;
+        coordsDisplay.innerHTML = `X: ${Math.round(lng)} | Z: ${Math.round(lat)}`;
+    });
+}
 
-        const gameX = Math.round(lng);
-        const gameZ = Math.round(lat);
+function createPopupContent(item, type) {
+    const isTracked = localStorage.getItem('track-' + item.id) === 'true';
+    
+    let content = "";
+    if (item.image) {
+        content += `<img src="${item.image}" class="popup-img">`;
+    }
 
-        // Refresh 
-        coordsDisplay.innerHTML = `X: ${gameX} | Z: ${gameZ}`;
+    content += `<b>${item.name}</b>
+                <hr style="border: 0.5px solid #233554; margin: 5px 0;">
+                ${item.coordsinfo ? `<small>${item.coordsinfo}</small><br>` : ''}
+                <p>${item.info}</p>
+                <div class="popup-footer">
+                    <label class="track-container">
+                        <input type="checkbox" ${isTracked ? 'checked' : ''} 
+                               onchange="toggleTrack('${item.id}', this.checked, '${type}')">
+                        <span class="track-text">Track</span>
+                    </label>
+                </div>`;
+    return content;
+}
+
+// Function to load the markers from the data.js
+function loadMarkers() {
+    mapData.lifepods.forEach(lifepod => {
+        const isTracked = localStorage.getItem('track-' + lifepod.id) === 'true';
+
+        const marker = L.marker(lifepod.coords, { 
+            icon: lifepodIcon,
+            opacity: isTracked ? 0.4 : 1.0
+        }).addTo(map);
+
+        allMarkers[lifepod.id] = marker;
+
+        marker.bindPopup(() => createPopupContent(lifepod, 'lifepod'), {
+            maxWidth: 300,
+            minWidth: 200
+        });
+
+        marker.bindTooltip(lifepod.name, { direction: 'right', opacity: 0.9 });
     });
 }
 
